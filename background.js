dc89f3c6c6c98f721bc1f93ad8c3ee339e6c2b98
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-07-19 19:43:14 +0530

    Updated the background.js file and created the popup.html file

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
 chrome.tabs.onActivated.addListener(tab=>{
-    console.log(tab);
-})
+    chrome.tabs.get(tab.tabId, current_tab_info =>{
+        console.log("Title: "+current_tab_info.title );
+        console.log("URL: "+  current_tab_info.url);
+    });
+});
+
+//  chrome.tabs.onUpdated.addListener(tab=>{
+//         chrome.tabs.get(tab.tabId, current_tab_info =>{
+//             console.log(current_tab_info.title)
+//         });
+//  });
