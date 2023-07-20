daad388a1f4e95b788fcccbf37dee6bea89ac904
bhavyasingh75 <bhavya.singh220@gmail.com>
2023-07-20 18:42:29 +0530

    added bookmark feature, waiting for review

--- background.js
+++ background.js
@@ -1,13 +1,73 @@
 function updatePopupWithTabInfo(title, url) {
     const titleElement = document.getElementById("title");
     const urlElement = document.getElementById("url");
-
-    if(titleElement && urlElement){
+    const bookmarkBtn = document.getElementById("bookmarkBtn");
+    const bookmarkedList = document.getElementById("bookmarkedList");
+    
+    if(titleElement && urlElement && bookmarkBtn){
         titleElement.textContent = "Title: " + title;
         urlElement.textContent = "URL: " + url;
-    }
-    
+
+        //check if the current URL is bookmarked
+        chrome.bookmarks.search({ url: url }, (bookmarks) => {
+            if(bookmarks.length > 0){
+                // link is bookmarked
+                bookmarkBtn.textContent = "Bookmarked";
+                bookmarkBtn.disabled = true;
+            } else{
+                //link not bookmarked
+                bookmarkBtn.textContent = "Bookmark this page";
+                bookmarkBtn.disabled = false;
+            }
+        });
+
+        // Add click event to bookmark button
+        bookmarkBtn.onclick = function () {
+            // Save the bookmark using the chrome.bookmarks API
+            chrome.bookmarks.create({ title: title, url: url }, (newBookmark) => {
+            console.log("Bookmark created with ID: " + newBookmark.id);
+            // Update the button state after bookmarking
+            bookmarkBtn.textContent = "Bookmarked";
+            bookmarkBtn.disabled = true;
+            });
+        };
+
+
+        //fetch and display bookmarked pages
+        chrome.bookmarks.getTree((bookmarkTree) => {
+            const bookmarkList = getBookmarkList(bookmarkTree);
+            bookmarkedList.innerHTML = bookmarkList;
+        });
+     };
 }
+    
+function getBookmarkList(bookmarkTree){
+    let listHTML = "<ul>";
+    for (let i = bookmarkTree.length - 1; i >= 0; i--) {
+      const node = bookmarkTree[i];
+     
+      if (node.children) {
+        listHTML += `<li><b>${node.title}</b></li>`;
+        listHTML += getBookmarkList(node.children);
+      } else {
+        listHTML += `<li><a href="${node.url}" target="_blank">${node.title}</a></li>`;
+      }
+    }
+    listHTML += "</ul>";
+    return listHTML;
+}
+    //ascending order
+      // let listHTML = "<ul>";
+    // bookmarkTree.forEach((node ) => {
+    //     if(node.children){
+    //         listHTML += `<li><b>${node.title}</b></li>`;
+    //         listHTML += getBookmarkList(node.children);
+    //     } else {
+    //         listHTML += `<li><a href="${node.url}" target="_blank">${node.title}</a></li>`;
+    //     }
+    // });
+    // listHTML += "</ul>";
+    // return listHTML;
 
 // get current active tab & update popup when it's loaded
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
