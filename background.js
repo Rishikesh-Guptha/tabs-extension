f0495919d6bf94a6ce5ad5d50945645042977a90
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-01 23:24:51 +0530

    Modified some code bases

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+//Firebase Configurations
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLSX15gty8jTRPPe4zwGvQy3BxJxIU9gQ",
+  authDomain: "surf-sentinel-fc8a9.firebaseapp.com",
+  projectId: "surf-sentinel-fc8a9",
+  storageBucket: "surf-sentinel-fc8a9.appspot.com",
+  messagingSenderId: "7666181085",
+  appId: "1:7666181085:web:c56f28e996bd8fd3d5cc34",
+  measurementId: "G-3FHBWM8CPD"
+};
+
 //BOOKMARKS
 
 function updatePopupWithTabInfo(title, url) {
