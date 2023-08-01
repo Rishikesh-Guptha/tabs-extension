e80d6655f1220b3e033a49154cd41f04893b3692
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-08-01 20:23:21 +0530

    Added the new firebase.js file

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLSX15gty8jTRPPe4zwGvQy3BxJxIU9gQ",
+  authDomain: "surf-sentinel-fc8a9.firebaseapp.com",
+  projectId: "surf-sentinel-fc8a9",
+  storageBucket: "surf-sentinel-fc8a9.appspot.com",
+  messagingSenderId: "7666181085",
+  appId: "1:7666181085:web:c56f28e996bd8fd3d5cc34",
+  measurementId: "G-3FHBWM8CPD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
